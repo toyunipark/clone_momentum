5a01d78240ecfe06c10198bcdf65274884b041b0
toyunipark <58938738+toyunipark@users.noreply.github.com>
2019-12-23 22:56:04 +0900

    Finished Project (dated on 12/23)
    Updated CSS details on the project.

--- clock.js
+++ clock.js
@@ -2,16 +2,16 @@ const clockContainer=document.querySelector(".js-clock"),
     clockTitle=clockContainer.querySelector("h1");
 
 
-    
 function getTime(){
     const date=new Date();
-    const hours=date.getHours();
+    var hour=date.getHours();
+    hours = hour % 12;
     const minutes=date.getMinutes();
     const seconds=date.getSeconds();
     clockTitle.innerText
-    =`${hours<10? `0${hours}`:hours}:${minutes<10? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`: seconds}`;
-    
+    =`${hour>12? `PM`:`AM`}  ${hours<10? `0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10 ?`0${seconds}`: seconds}`;
 }
+
 
 function init(){
     getTime();
@@ -19,3 +19,6 @@ function init(){
 }
 
 init();
+
+
+
--- weather.js
+++ weather.js
@@ -9,7 +9,7 @@ function getWeather(lat, lng){
     }).then(function(json){
         const temperature = json.main.temp;
         const place = json.name;
-        weather.innerText=`${temperature}@${place}`
+        weather.innerText=`${temperature}°C @ ${place}`
     });
 
 }
